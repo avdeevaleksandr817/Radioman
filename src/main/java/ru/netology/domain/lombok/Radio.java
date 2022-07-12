05feb31942ae45ceca704bcac5932b908ca3c44e
avdeevaleksandr <avdeevaleksandr817@gmail.com>
2022-07-13 01:18:59 +0400

    added lombok

---
+++ src/main/java/ru/netology/domain/lombok/Radio.java
@@ -0,0 +1,63 @@
+package ru.netology.domain.lombok;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Radio {
+    public int currentStation;
+    public int currentVolume;
+    public int minStation = 0;
+    public int maxStation = 10;
+    public int minVolume = 0;
+    public int maxVolume = 100;
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation > maxStation) {
+            return;
+        }
+        if (currentStation < minStation) {
+            return;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public void nextStation() {
+        if (currentStation == maxStation) {
+            currentStation = minStation;
+            return;
+        }
+        currentStation++;
+    }
+
+    public void prevStation() {
+        if (currentStation == minStation) {
+            currentStation = maxStation;
+            return;
+        }
+        currentStation--;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume > maxVolume) {
+            return;
+        }
+        if (currentVolume < minVolume) {
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+
+    public void increaseCurrentVolume() {
+        if (currentVolume < maxVolume)
+            currentVolume++;
+    }
+
+    public void decreaseCurrentVolume() {
+        if (currentVolume > minVolume)
+            currentVolume--;
+    }
+}
